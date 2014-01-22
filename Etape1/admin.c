2da5f6d9c08002bc31446ca1755eb260401a5f93
bendem <online@bendem.be>
2014-01-22 17:16:09 +0100

    Fix spacing

--- Etape1/admin.c
+++ Etape1/admin.c
@@ -2,46 +2,45 @@
 
 char LocalReadChar()
 {
- char Tampon[80] ;
- fgets(Tampon,sizeof Tampon,stdin ) ;
- return Tampon[0] ;
+    char Tampon[80];
+    fgets(Tampon, sizeof Tampon, stdin);
+    return Tampon[0];
 }
 
 int main(void)
 {
- char c;
- int res ;
- int Heure, HeureS,HeureP ;
- int PlacesLibres ;
- char Tampon[50] ;
- int NumTicket ;
+    char c;
+    int res;
+    int Heure, HeureS, HeureP;
+    int PlacesLibres;
+    char Tampon[50];
+    int NumTicket;
 
- while(1)
- {
- printf ("menu :\n") ;
- printf("1) Creation du fichier  \n") ;
- printf("2) Affichage du fichier \n") ;
- printf("3) Reservation Ticket \n") ;
- printf("4) Recherche et paiement \n") ;
- printf("5) Recherche et sortie \n") ;
- printf("6) exit\n") ;
- printf("---------------------\n") ;
- c = LocalReadChar() ;	/* Readchar se trouve dans la librairie Physlib */
- printf( "\n%c\n",c) ;
- switch(c)
- {
-   case '1' :
-              res = CreationFichierTransaction( "essai" , 50 ) ;
-	      printf("Resultat %d \n",res) ;
-	      break ;
-   case '2' :
-             res = AffichageFichier("essai") ;
-	     printf("Resultat %d \n",res) ;
-	     break ;
-   case '6' :
-
-           exit(1) ;
-  }
- }
- return(1) ;
+    while(1)
+    {
+        printf ("menu :\n");
+        printf("1) Creation du fichier  \n");
+        printf("2) Affichage du fichier \n");
+        printf("3) Reservation Ticket \n");
+        printf("4) Recherche et paiement \n");
+        printf("5) Recherche et sortie \n");
+        printf("6) exit\n");
+        printf("---------------------\n");
+        c = LocalReadChar();   /* Readchar se trouve dans la librairie Physlib */
+        printf("\n%c\n", c);
+        switch(c)
+        {
+            case '1' :
+            res = CreationFichierTransaction("essai", 50);
+            printf("Resultat %d \n", res);
+            break;
+            case '2' :
+            res = AffichageFichier("essai");
+            printf("Resultat %d \n", res);
+            break;
+            case '6' :
+            exit(1);
+        }
+    }
+    return(1);
 }
