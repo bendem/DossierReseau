27cff097a4e85b883488280bbc2142566c2a0a84
bendem <online@bendem.be>
2014-02-10 10:12:06 +0100

    CS

--- protocol/protocol.h
+++ protocol/protocol.h
@@ -8,35 +8,33 @@ static int Liaison;
 typedef enum { data, ack, nak } type_trame;
 
 enum TypeDNS {
-               TREQUETEDNS=1,
-               TREPONSEDNS=2
-	     };
-
-typedef char NomDNS[80] ;
-
-struct RequeteDNS{
-enum TypeDNS Type;
-int Numero;
-int IP;
-NomDNS Nom;
+    TREQUETEDNS = 1,
+    TREPONSEDNS = 2
 };
 
-struct Segment
-{
- int	PortSource;
- int	PortDestination;
- char   Buffer[TBUFFERTRANSPORT];
+typedef char NomDNS[80];
+
+struct RequeteDNS {
+    enum TypeDNS Type;
+    int Numero;
+    int IP;
+    NomDNS Nom;
+};
+
+struct Segment {
+    int PortSource;
+    int PortDestination;
+    char   Buffer[TBUFFERTRANSPORT];
 };
 
 #define TBUFFERPAQUET  sizeof(struct Segment)
 
-struct Paquet
-{
- int  IPSource;
- int  IPDestination;
- int  Protocole; /* le champ type */
- int  Numero;
- char Buffer[TBUFFERPAQUET];
+struct Paquet {
+    int  IPSource;
+    int  IPDestination;
+    int  Protocole; /* le champ type */
+    int  Numero;
+    char Buffer[TBUFFERPAQUET];
 };
 
 #define ICMP 1
@@ -48,23 +46,21 @@ struct Paquet
 
 #define TBUFFERTRAME  sizeof(struct Paquet)
 
-struct mac
-{
- short int Constructeur;
- int       Numero;
+struct mac {
+    short Constructeur;
+    int   Numero;
 };
 
-struct Trame
-{
- int  Numero;
- type_trame type;
- struct mac MacSource;
- struct mac MacDestination;
- int Flag;
- int seq;
- int  ack;
- char Buffer[TBUFFERTRAME];
+struct Trame {
+    int  Numero;
+    type_trame type;
+    struct mac MacSource;
+    struct mac MacDestination;
+    int  Flag;
+    int  seq;
+    int  ack;
+    char Buffer[TBUFFERTRAME];
 };
 
-#define inc(k) if(k<MAX_SEQ)k=k+1;else k=0;
+#define inc(k) if(k < MAX_SEQ) k = k+1; else k = 0;
 #endif
