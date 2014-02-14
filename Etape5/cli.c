d0da58742c9b8283c001695f96a5fc22b96dccb5
bendem <online@bendem.be>
2014-02-14 14:34:28 +0100

    cs

--- Etape5/cli.c
+++ Etape5/cli.c
@@ -104,29 +104,28 @@ int RequeteReservationBDEF(char* Fichier, int heure){
     alarm(30);
 
     for(;;){
-	    rc = ReceiveDatagram(Desc, &notreRequetePerso, sizeof(struct RequeteBDEF), &psor);
-	    if (rc == -1) {
-	        if (errno == EINTR && IsSigAlarm == 1) {
-	        	IsSigAlarm = 0;
-	        	return RequeteReservationBDEF(Fichier, heure);
-	        }
+        rc = ReceiveDatagram(Desc, &notreRequetePerso, sizeof(struct RequeteBDEF), &psor);
+        if (rc == -1) {
+            if (errno == EINTR && IsSigAlarm == 1) {
+                IsSigAlarm = 0;
+                return RequeteReservationBDEF(Fichier, heure);
+            }
 
-	        perror("ReceiveDatagram");
-	        return -1;
-	    } else {
-	        if (NumTransac!=notreRequetePerso.NumTransac)
-	        {
-	        	continue;
-	        }
-	        fprintf(stderr, "bytes:%d:%d\n", rc, notreRequetePerso.NumeroTicket);
-	        if (notreRequetePerso.NumeroTicket > 0) {
-	            ReservationTicketBDEF(Fichier, GetIP(&psoo), GetPort(&psoo), NumTransac, notreRequetePerso.Heure, NULL);
-	            NumTransac++;
-	        }
+            perror("ReceiveDatagram");
+            return -1;
+        } else {
+            if (NumTransac!=notreRequetePerso.NumTransac) {
+                continue;
+            }
+            fprintf(stderr, "bytes:%d:%d\n", rc, notreRequetePerso.NumeroTicket);
+            if (notreRequetePerso.NumeroTicket > 0) {
+                ReservationTicketBDEF(Fichier, GetIP(&psoo), GetPort(&psoo), NumTransac, notreRequetePerso.Heure, NULL);
+                NumTransac++;
+            }
 
-	        return notreRequetePerso.NumeroTicket;
-	    }
-	}
+            return notreRequetePerso.NumeroTicket;
+        }
+    }
 }
 
 
@@ -152,5 +151,5 @@ int RecoverFichierTransactionBDEF(char* NomFichier){
 }
 
 void HandlerSigAlarm(int sig){
-	IsSigAlarm = 1;
+    IsSigAlarm = 1;
 }
